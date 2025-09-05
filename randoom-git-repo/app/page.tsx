9998eae13a789431d553cb96824ffe4b6d514b70
pulkiiiit <pulkitshukla001@gmail.com>
2025-09-06 04:10:31 +0530

    Add language selection dropdown and display message functionality

--- randoom-git-repo/app/page.tsx
+++ randoom-git-repo/app/page.tsx
@@ -1,17 +1,43 @@
 "use client";
 import { ChevronDown } from "lucide-react";
 import { useState } from "react";
+import React from "react";
+import { languages } from "@/lib/languaes";
 
 export default function Home() {
   const [language, setLanguage] = useState<string | null>("Select language");
   const [isopen, setIsOpen] = useState(false);
+  const [displaymessage, setDisplayMessage] = useState("Please select a language");
+
+  function changeTheTitle(selectedLanguage: string) {
+    setLanguage(selectedLanguage);
+    setIsOpen(false);
+    setDisplayMessage(`Fetching random repo for  ${selectedLanguage}`);
+  }
+  function toggledropdown() {
+    setIsOpen(!isopen);
+    setDisplayMessage("Please select a language");
+  }
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-3xl font-bold ">Random github repositary</h1>
       <p className="mt-10">Select a random repo</p>
       <div className="flex items-center mt-10 border-2 border-white rounded-md p-2">
-        <button className="p-2 cursor-pointer">{language}</button>
+        <button className="p-2 cursor-pointer" onClick={toggledropdown}>{language}</button>
         <ChevronDown className=" cursor-pointer w-5 h-5"/>
+      </div>
+      <div className="mt-5 border-2 border-white rounded-md p-2" style={{ display: isopen ? "block" : "none" }}>
+        {languages.map((l) => (
+          <div className=" mt-2 border-2 border-white rounded-md p-2">
+            <ul>
+              <li> <button className="w-full h-full" onClick={() => changeTheTitle(l)}>{l}</button></li>
+            </ul>
+          </div>
+        ))}
+      </div>
+      <div className="mt-10 bg-amber-50 text-amber-950 p-10 rounded-md">
+        {displaymessage}
       </div>
     </div>
   );
---
+++ randoom-git-repo/lib/languaes.ts
@@ -0,0 +1,6 @@
+export const languages = [
+    "JavaScript",
+    "Python",
+    "Java",
+    "C#",
+]
